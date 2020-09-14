198660dfa97e11b2bcd6c59c0064acb339c8e34b
Рустам Каюмов <krr@pkzdrav.ru>
2020-09-15 01:37:44 +0500

    Кастомный Models Manager

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,14 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models here.
 
+
+class PublishedManager(models.Manager):
+    """Кастомный models manager"""
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
 class Post(models.Model):
+
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published')
@@ -17,11 +24,14 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft', verbose_name='Статус публикации')
 
+    objects = models.Manager() # Менеджер по умолчанию
+    published = PublishedManager() # Кастомный менеджер
+
+
     class Meta:
         ordering = ('-publish',)
         verbose_name = 'Пост'
         verbose_name_plural = 'Посты'
-
 
     def __str__(self):
         return self.title

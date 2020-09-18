205c83a4625e98642fbee7012427f6873a98540c
Rustam <rustrnb@yandex.ru>
2020-09-18 23:19:54 +0500

    Добавление модели комментариев

--- blog/models.py
+++ blog/models.py
@@ -40,3 +40,18 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year,
                                                  self.publish.month, self.publish.day, self.slug])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', verbose_name='Пост')
+    name = models.CharField(max_length=80, verbose_name='Заголовок комментария')
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created')
+
+    def __str__(self):
+        return f'Комментарии {self.name} к посту {self.post}'

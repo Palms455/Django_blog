a6c7528199fc9487c77f55201b2324fd383680b8
Rustam <rustrnb@yandex.ru>
2020-09-20 00:18:01 +0500

    Добавление польз. фильтров на примере Markdown

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -2,6 +2,10 @@ from django import template
 from ..models import Post
 from django.db.models import Count
 
+from django.utils.safestring import mark_safe
+import markdown
+
+
 register = template.Library()
 # для регистрации пользовательских тегов
 
@@ -22,3 +26,7 @@ def show_latest_posts(count=5):
 def get_most_commented_posts(count=5):
     return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
 
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
+# mark_safe помещает содержимое в кач-ве HTML кода
